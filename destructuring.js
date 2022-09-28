a67dcf7c5b7b98641b32992ef8c18e0b1fd6f7e0
chris-townsend <chris-townsend@live.co.uk>
2022-09-28 09:17:41 +0000

    Add notes for advanced js destructuring

--- destructuring.js
+++ destructuring.js
@@ -5,8 +5,70 @@
 
 // Destructuring arrays
 
+let ages = [30, 26, 27];
+
+//let John = ages[0];
+//let Mary = ages[1];
+//let Joe = ages[2];
+
+let [John, Mary, Joe] = ages;
+console.log(John, Mary, Joe);
+
 // Destructuring objects
+
+let jobs = {
+    Mike: 'designer',
+    Jill: 'developer',
+    Jack: 'accountant'
+};
+
+let {Mike, Jill, Jack} = jobs;
+
+console.log(Mike, Jill, Jack)
 
 // Destructuring subsets
 
+let languages = ['english', 'french', 'spanish', 'german', 'japanese']
+
+let [johnNative, johnSecondary] = languages;
+
+console.log(johnNative, johnSecondary);
+
+// using commas at the start skips the first two languages in the array 
+let [, , maryNative, marySecondary] = languages;
+
+console.log(maryNative, marySecondary);
+
+let languages2 = {
+    firstLanguage: 'english',
+    secondLanguage: 'french',
+    thirdLanguage: 'spanish',
+    fourthLanguage: 'german'
+};
+
+let {firstLanguage, thirdLanguage } = languages2;
+console.log(firstLanguage, thirdLanguage);
+
 // Using rest parameter syntax
+
+
+let fruits = ['apple', 'orange', 'banana', 'peach', 'cherry'];
+
+let [favourite, secondFavourite, ...others] = fruits;
+
+console.log(favourite);
+console.log(secondFavourite);
+console.log(others);
+
+let favouriteFoods = {
+    brian: 'pizza',
+    sarah: 'pasta',
+    sam: 'cheese',
+    anna: 'steak'
+}
+
+let {brian, sarah, ...rest} = favouriteFoods;
+
+console.log(brian);
+console.log(sarah);
+console.log(rest);
